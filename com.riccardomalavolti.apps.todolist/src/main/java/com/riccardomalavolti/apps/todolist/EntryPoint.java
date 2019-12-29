999ba1d0a0a5cfde9aa91d6dd2d30214e0f7e94b
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2019-12-29 18:26:59 +0100

    Updated initial Maven configuration

---
+++ com.riccardomalavolti.apps.todolist/src/main/java/com/riccardomalavolti/apps/todolist/EntryPoint.java
@@ -0,0 +1,10 @@
+package com.riccardomalavolti.apps.todolist;
+
+
+public class EntryPoint 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "~~~~~~ TODO List ~~~~~~" );
+    }
+}
