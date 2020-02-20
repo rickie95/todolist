e0c6acec64a30f22c78c35fb0a5ed1c7f7733305
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-02-20 14:53:01 +0100

    chore: Created entry point and added Log4J dependency

---
+++ com.riccardomalavolti.apps.todolist/src/main/java/com/riccardomalavolti/apps/todolist/main/Main.java
@@ -0,0 +1,16 @@
+package com.riccardomalavolti.apps.todolist.main;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+	private static final Logger LOGGER = LogManager.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		
+		LOGGER.info("The application is starting.");
+
+	}
+
+}
