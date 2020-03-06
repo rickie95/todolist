7604ef55a8724a076de20c357f7c2348108eb66e
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-03-06 16:21:17 +0100

    refactor: code smell

--- com.riccardomalavolti.apps.todolist/src/main/java/com/riccardomalavolti/apps/todolist/model/Tag.java
+++ com.riccardomalavolti.apps.todolist/src/main/java/com/riccardomalavolti/apps/todolist/model/Tag.java
@@ -66,8 +66,9 @@ public class Tag {
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!id.equals(other.id)) {
 			return false;
+		}
 		return true;
 	}
 	
