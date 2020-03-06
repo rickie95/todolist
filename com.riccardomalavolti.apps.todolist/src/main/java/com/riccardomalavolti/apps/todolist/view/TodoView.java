910730490c8fb54f74e266d75f89da946e4ca754
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-03-06 15:51:06 +0100

    chore: added TodoView interface

---
+++ com.riccardomalavolti.apps.todolist/src/main/java/com/riccardomalavolti/apps/todolist/view/TodoView.java
@@ -0,0 +1,31 @@
+package com.riccardomalavolti.apps.todolist.view;
+
+import java.util.List;
+
+import com.riccardomalavolti.apps.todolist.model.Tag;
+import com.riccardomalavolti.apps.todolist.model.Todo;
+
+public interface TodoView {
+
+	public void showAllTodo(List<Todo> list);
+
+	public void showAllTags(List<Tag> tags);
+	
+	public void updateTagView(List<Tag> tagList);
+
+	public void updateTodoView(List<Todo> todoList);
+
+	public void updateTodo(Todo todo);
+
+	public void updateTag(Tag tag);
+
+	public void removeTodo(Todo todo);
+	
+	public void removeTag(Tag tag);
+	
+	public void error(String message);
+
+	public void addTodo(Todo todo);
+
+	public void addTag(Tag tag);
+}
