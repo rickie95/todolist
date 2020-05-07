44e9aee6ecb9e9de9a820d82af7a6c00540fb4e1
Riccardo Malavolti <riccardo.malavolti@gmail.com>
2020-05-07 11:49:51 +0200

    chore: removed unused import

--- com.riccardomalavolti.apps.todolist/src/main/java/com/riccardomalavolti/apps/todolist/TodoList.java
+++ com.riccardomalavolti.apps.todolist/src/main/java/com/riccardomalavolti/apps/todolist/TodoList.java
@@ -1,7 +1,6 @@
 package com.riccardomalavolti.apps.todolist;
 
 import java.awt.EventQueue;
-import java.net.ConnectException;
 import java.util.concurrent.Callable;
 
 import org.apache.logging.log4j.Level;
@@ -27,20 +26,18 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-
 @Command(mixinStandardHelpOptions = true)
-public class TodoList implements Callable<Void>{
+public class TodoList implements Callable<Void> {
 
 	@Option(names = { "--db-name" }, description = "Database name")
 	private String databaseName = TodoRepositoryMongoDB.DB_NAME;
-	
+
 	@Option(names = { "--collection-name" }, description = "Name of the mongo collection used.")
 	private String collectionName = TodoRepositoryMongoDB.COLLECTION_NAME;
-	
-	@Option(names = {"--verbosity", "-v" }, description = "Specifies verbosity level: [ info | debug ]")
+
+	@Option(names = { "--verbosity", "-v" }, description = "Specifies verbosity level: [ info | debug ]")
 	private String verbosityLevel = "info";
-	
-	
+
 	private static final Logger LOGGER = LogManager.getLogger(TodoList.class);
 
 	public static void main(String[] args) {
@@ -49,51 +46,47 @@ public class TodoList implements Callable<Void>{
 
 	@Override
 	public Void call() throws Exception {
-		
+
 		EventQueue.invokeLater(() -> {
-			
+
 			MongoClient mongoClient = null;
 			TagRepository tagMongoRepo = null;
 			TodoRepository todoMongoRepo = null;
 			TodoController todoController = null;
 			MainView todoView = null;
-			
+
 			LoggerContext context = (LoggerContext) LogManager.getContext(false);
 			Configuration config = context.getConfiguration();
 			LoggerConfig rootConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
 			rootConfig.setLevel(Level.INFO);
-			
-			if(verbosityLevel.equals("debug"))
-					rootConfig.setLevel(Level.DEBUG);
-			
-		
+
+			if (verbosityLevel.equals("debug"))
+				rootConfig.setLevel(Level.DEBUG);
+
 			LOGGER.info("The application is starting.");
-			
+
 			try {
 				mongoClient = new MongoClient(new ServerAddress("localhost"));
 				tagMongoRepo = new TagRepositoryMongoDB(mongoClient);
 				todoMongoRepo = new TodoRepositoryMongoDB(mongoClient, tagMongoRepo);
 				TodoManager todoManager = new TodoManager(todoMongoRepo, tagMongoRepo);
-				
-				
+
 				todoView = new MainView();
 				todoView.setVisible(true);
-				
+
 				todoController = new TodoController(todoView, todoManager);
-			}catch(MongoSocketOpenException ex) {
+			} catch (MongoSocketOpenException ex) {
 				LOGGER.fatal("Cannot find Mongo instance on localhost:27017.");
 				MessageBoxFactory.showErrorMessage(null, "Cannot find mongo!");
-			}catch(RuntimeException ex) {
+			} catch (RuntimeException ex) {
 				LOGGER.fatal("runtime");
 			}
-			
-			
-			
+
 			todoView.setController(todoController);
-			
+
 			LOGGER.info("Todolist is ready");
 		});
-		
+
 		return null;
 	}
 
